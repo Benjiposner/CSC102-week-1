9092dd788f143d2e24636e615e1b541ee11dfeee
Benjiposner <BPOSNER90671@uat.edu>
2024-11-07 15:24:10 -0600

    Update palindrome_strings.js

--- palindrome_strings.js
+++ palindrome_strings.js
@@ -1,14 +1,13 @@
 function strings()
 {
-    var string1 = document.getElementById("firstStr").value
-    var string2 = document.getElementById("secondStr").value
+    var string1 = document.getElementById("firstStr").value;
     
     var splitStr = string1.split("");
-    document.getElementById("string info").innerHTML = splitStr
+    document.getElementById("string info").innerHTML = splitStr;
     var reverseStr = splitStr.reverse();
-    document.getElementById("string info").innerHTML = reverseStr
+    document.getElementById("string info").innerHTML = reverseStr;
     var joinStr = reverseStr.join("");
-    document.getElementById("string info").innerHTML = joinStr
+    document.getElementById("string info").innerHTML = joinStr;
 
     if (string1 == joinStr)
     {
